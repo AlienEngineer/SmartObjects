f95ad505d5de40e8b1a5ae7d7b60dbcf58f508de
AlienEngineer <sergio.ferreira@celfinet.com>
2016-11-26 15:41:01 +0000

    use cases

--- SmartObjects/SmartObjects.Tests/UseCases.cs
+++ SmartObjects/SmartObjects.Tests/UseCases.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using NUnit.Framework;
 
 namespace SmartObjects.Tests
@@ -8,14 +9,38 @@ namespace SmartObjects.Tests
     [TestFixture]
     class UseCases
     {
+        private int MAX_NUMBER_OF_ITERATIONS = 1000000;
         private const int NUMBER_OF_OBJECTS_PER_ITERATION = 100;
 
+        //[Test]
+        public void ExportTimes()
+        {
+            var file = new StreamWriter(File.Create(@"C:\MyProjects\SmartObjects\Test.csv"));
+            for (int numberOfIterations = 0; numberOfIterations < MAX_NUMBER_OF_ITERATIONS; numberOfIterations += 10000)
+            {
+
+                // Arrange
+                var recycleBin = new RecycleBin<object>(NUMBER_OF_OBJECTS_PER_ITERATION);
+                var recycleCollection = new RecycleCollection<object>(recycleBin, new List<object>(NUMBER_OF_OBJECTS_PER_ITERATION));
+                var regularCollection = new List<object>(NUMBER_OF_OBJECTS_PER_ITERATION);
+
+                // Act
+                var recycleCollectionTime = Execute(numberOfIterations, regularCollection, () => new object());
+                var regularCollectionTime = Execute(numberOfIterations, recycleCollection, recycleBin.GetInstance);
+
+                // Assert
+                file.WriteLine($"{numberOfIterations};{recycleCollectionTime};{regularCollectionTime}");
+            }
+            file.Flush();
+            file.Close();
+        }
 
         [Test]
-        public void When_creating_and_releasing_100_objects_over_10000_iterations_it_should_be_quicker_to_use_a_recycle_collection()
+        public void When_creating_and_releasing_100_objects_over_x_iterations_it_should_be_quicker_to_use_a_recycle_collection(
+            [Values(100, 1000, 10000)]
+            int numberOfIterations)
         {
             // Arrange
-            var numberOfIterations = 10000;
             var recycleBin = new RecycleBin<object>(NUMBER_OF_OBJECTS_PER_ITERATION);
             var recycleCollection = new RecycleCollection<object>(recycleBin, new List<object>(NUMBER_OF_OBJECTS_PER_ITERATION));
             var regularCollection = new List<object>(NUMBER_OF_OBJECTS_PER_ITERATION);
@@ -25,11 +50,12 @@ namespace SmartObjects.Tests
             var regularCollectionTime = Execute(numberOfIterations, recycleCollection, recycleBin.GetInstance);
 
             // Assert
-            Assert.That(regularCollectionTime, Is.GreaterThan(recycleCollectionTime));
             Console.WriteLine($@"
-    Recycle: {recycleCollectionTime}
-    Regular: {regularCollectionTime}
+    Iterations: {numberOfIterations}
+    Recycle:    {recycleCollectionTime}
+    Regular:    {regularCollectionTime}
 ");
+            Assert.That(regularCollectionTime, Is.GreaterThan(recycleCollectionTime));
         }
 
         private static long Execute(int numberOfIterations, ICollection<object> collection, Func<object> objectMake)
@@ -48,7 +74,7 @@ namespace SmartObjects.Tests
             }
 
             stopwatch.Stop();
-            return stopwatch.ElapsedMilliseconds;
+            return stopwatch.ElapsedTicks;
         }
     }
 }
